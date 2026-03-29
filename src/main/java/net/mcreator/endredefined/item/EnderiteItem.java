bfc64ee4b1fa4ef8875e678599b8e02c776ccb9b
DoomingTheCube <marsteo@gmx.de>
2026-03-29 15:22:55 +0100

    Delete src/main directory
    deleting3

--- src/main/java/net/mcreator/endredefined/item/EnderiteItem.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.endredefined.item;
-
-import net.minecraft.world.item.Item;
-
-public class EnderiteItem extends Item {
-	public EnderiteItem(Item.Properties properties) {
-		super(properties);
-	}
-}
